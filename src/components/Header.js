202ba096d38fc731892b72fcd180e95e2fef08ae
naiiiden <na4ko359@gmail.com>
2022-07-23 16:24:50 +0300

    mobile navigation now opens/closes

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,37 @@
+import { useState } from "react";
 import header_logo from "../images/logo/logo-light.svg"; 
 
 const Header = () => {
+    const [openMenu, setOpenMenu] = useState(false);
+
     return (
         <header className="header">
             <img src={header_logo} alt="Ecwid's logo" className="header--logo"/>
-            <button className="mobile--navigation--button" aria-label="toggle navigation">
+            <button className="mobile--navigation--button" aria-label="toggle navigation" onClick={() => setOpenMenu(!openMenu)}>
                 <div className="button--inner--div"></div>
             </button>
+            <nav className="header--nav">
+                <ul className={`header--nav--ul ${openMenu ? "show" : ""}`}>
+                    <li>
+                        <span>Sell</span>
+                    </li>
+                    <li>
+                        <span>Market</span>
+                    </li>
+                    <li>
+                        <span>Manage</span>
+                    </li>
+                    <li>
+                        <span>Learn</span>
+                    </li>
+                    <li>
+                        <span>Partners</span>
+                    </li>
+                    <li><a href="#" className="link">Pricing</a></li>
+                    <li><a href="#" className="get--started--link link">Get started for FREE</a></li>
+                    <li><a href="#" className="login--link link">Login</a></li>
+                </ul>
+            </nav>
         </header>
     )
 }
