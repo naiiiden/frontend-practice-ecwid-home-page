35f3f787f0edeae6693f32e64d9f511009faa54b
naiiiden <na4ko359@gmail.com>
2022-07-25 19:38:58 +0300

    fix webpack compile error

--- src/components/Header.js
+++ src/components/Header.js
@@ -38,7 +38,7 @@ const Header = () => {
         })
         window.addEventListener("resize", updateMediaDesktop);
         return () => window.removeEventListener("resize", updateMediaDesktop);
-    }, [isDesktop]);
+    }, [isDesktop, scroll]);
 
     return (
         <>
