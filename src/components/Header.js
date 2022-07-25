b57e749d516b9447f5ca912665b5ff7e23acab6f
naiiiden <na4ko359@gmail.com>
2022-07-25 07:37:57 +0300

    header component fill empty <a> hrefs

--- src/components/Header.js
+++ src/components/Header.js
@@ -45,92 +45,92 @@ const Header = () => {
             <nav className="header--nav">
                 {isTablet && !isDesktop ? 
                 <ul className="nav768-1280--ul">
-                    <li><a href="#">Sell</a></li>
-                    <li><a href="#">Market</a></li>
-                    <li><a href="#">Manage</a></li>
-                    <li className="get--started--li2"><a href="#">Get Started for FREE</a></li>
+                    <li><a href="https://www.ecwid.com/sell">Sell</a></li>
+                    <li><a href="https://www.ecwid.com/promote">Market</a></li>
+                    <li><a href="https://www.ecwid.com/manage">Manage</a></li>
+                    <li className="get--started--li2"><a href="https://my.ecwid.com/cp/#register">Get Started for FREE</a></li>
                 </ul> : ""}
                 <ul className={`header--nav--ul ${openMenu ? "show" : ""}`}>
                 {!isDesktop ? 
                     <li>
                         <span onClick={() => setOpenSell(!openSell)} className={`${openSell ? "open" : ""}`} role="button">Sell</span>
                         <ul className={`dropdown ${openSell ? "show" : ""}`}>
-                            <li><a href="#">Sell anywhere</a></li>
-                            <li><a href="#">Website</a></li>
-                            <li><a href="#">Instagram</a></li>
-                            <li><a href="#">TikTok</a></li>
-                            <li><a href="#">Facebook</a></li>
-                            <li><a href="#">Google</a></li>
-                            <li><a href="#">Amazon</a></li>
-                            <li><a href="#">POS</a></li>
-                            <li><a href="#">ShopApp</a></li>
+                            <li><a href="https://www.ecwid.com/sell">Sell anywhere</a></li>
+                            <li><a href="https://www.ecwid.com/sell">Website</a></li>
+                            <li><a href="https://www.ecwid.com/instagram">Instagram</a></li>
+                            <li><a href="https://www.ecwid.com/tiktok">TikTok</a></li>
+                            <li><a href="https://www.ecwid.com/facebook-commerce">Facebook</a></li>
+                            <li><a href="https://www.ecwid.com/sell-on-google">Google</a></li>
+                            <li><a href="https://www.ecwid.com/amazon">Amazon</a></li>
+                            <li><a href="https://www.ecwid.com/pos">POS</a></li>
+                            <li><a href="https://www.ecwid.com/shopapp">ShopApp</a></li>
                         </ul>
                     </li>
                     :
-                    <li><a href="#">Sell</a></li>
+                    <li><a href="https://www.ecwid.com/sell">Sell</a></li>
                 }
                 {!isDesktop ? 
                     <li>
                         <span onClick={() => setOpenMarket(!openMarket)} className={`${openMarket ? "open" : ""}`} role="button">Market</span>
                         <ul className={`dropdown ${openMarket ? "show" : ""}`}>
-                            <li><a href="#">Market your business</a></li>
-                            <li><a href="#">Google Ads</a></li>
-                            <li><a href="#">Facebook</a></li>
-                            <li><a href="#">Email</a></li>
+                            <li><a href="https://www.ecwid.com/promote">Market your business</a></li>
+                            <li><a href="https://www.ecwid.com/google-advertising">Google Ads</a></li>
+                            <li><a href="https://www.ecwid.com/facebook-remarketing">Facebook</a></li>
+                            <li><a href="https://www.ecwid.com/mailchimp">Email</a></li>
 
                         </ul>
                     </li>
                     :
-                    <li><a href="#">Market</a></li>
+                    <li><a href="https://www.ecwid.com/promote">Market</a></li>
                 }
                 {!isDesktop ? 
                     <li>
                         <span onClick={() => setOpenManage(!openManage)} className={`${openManage ? "open" : ""}`} role="button">Manage</span>
                         <ul className={`dropdown ${openManage ? "show" : ""}`}>
-                            <li><a href="#">Manage your store</a></li>
-                            <li><a href="#">Mobile</a></li>
-                            <li><a href="#">App Market</a></li>
-                            <li><a href="#">Discount Shipping</a></li>
+                            <li><a href="https://www.ecwid.com/manage">Manage your store</a></li>
+                            <li><a href="https://www.ecwid.com/ecwid-mobile">Mobile</a></li>
+                            <li><a href="https://www.ecwid.com/apps">App Market</a></li>
+                            <li><a href="https://www.ecwid.com/shipping-labels">Discount Shipping</a></li>
                         </ul>
                     </li>
                     :
-                    <li><a href="#">Manage</a></li>
+                    <li><a href="https://www.ecwid.com/manage">Manage</a></li>
                 }
                 {!isDesktop ? 
                     <li>
                         <span onClick={() => setOpenLearn(!openLearn)} className={`${openLearn ? "open" : ""}`} role="button">Learn</span>
                         <ul className={`dropdown ${openLearn ? "show" : ""}`}>
-                            <li><a href="#">Learn</a></li>
-                            <li><a href="#">Demo</a></li>
-                            <li><a href="#">Help Center</a></li>
-                            <li><a href="#">Blog</a></li>
-                            <li><a href="#">Customer Support</a></li>
-                            <li><a href="#">Guides</a></li>
-                            <li><a href="#">Ecwid Academy</a></li>
-                            <li><a href="#">Podcast</a></li>
-                            <li><a href="#">Tutorials</a></li>
+                            <li><a href="https://www.ecwid.com/blog">Learn</a></li>
+                            <li><a href="https://www.ecwid.com/watchdemo">Demo</a></li>
+                            <li><a href="https://support.ecwid.com/hc/en-us">Help Center</a></li>
+                            <li><a href="https://www.ecwid.com/blog">Blog</a></li>
+                            <li><a href="https://support.ecwid.com/hc/en-us/requests/new">Customer Support</a></li>
+                            <li><a href="https://www.ecwid.com/guides">Guides</a></li>
+                            <li><a href="https://ecommerce-academy.ecwid.com/">Ecwid Academy</a></li>
+                            <li><a href="https://www.ecwid.com/blog/podcast">Podcast</a></li>
+                            <li><a href="https://support.ecwid.com/hc/en-us/categories/360000015889-Ecwid-Video-Guides">Tutorials</a></li>
                         </ul>
                     </li>
                     :
-                    <li><a href="#">Learn</a></li>
+                    <li><a href="https://www.ecwid.com/blog">Learn</a></li>
                 }
                 {!isDesktop ? 
                     <li>
                         <span onClick={() => setOpenPartners(!openPartners)} className={`${openPartners ? "open" : ""}`} role="button">Partners</span>
                         <ul className={`dropdown ${openPartners ? "show" : ""}`}>
-                            <li><a href="#">Partners <WindowIcon className="window--icon"/></a></li>
-                            <li><a href="#">Resell <WindowIcon className="window--icon"/></a></li>
-                            <li><a href="#">Refer <WindowIcon className="window--icon"/></a></li>
-                            <li><a href="#">Build an App <WindowIcon className="window--icon"/></a></li>
-                            <li><a href="#">Find an expert <WindowIcon className="window--icon"/></a></li>
+                            <li><a href="https://www.ecwid.com/partners">Partners <WindowIcon className="window--icon"/></a></li>
+                            <li><a href="https://www.ecwid.com/partners/reseller">Resell <WindowIcon className="window--icon"/></a></li>
+                            <li><a href="https://www.ecwid.com/partners/referral">Refer <WindowIcon className="window--icon"/></a></li>
+                            <li><a href="https://www.ecwid.com/partners/app-market">Build an App <WindowIcon className="window--icon"/></a></li>
+                            <li><a href="https://www.ecwid.com/experts">Find an expert <WindowIcon className="window--icon"/></a></li>
                         </ul>
                     </li>
                     :
-                    <li><a href="#">Partners</a></li>
+                    <li><a href="https://www.ecwid.com/partners">Partners</a></li>
                 }
-                    <li className="pricing--li"><a href="#" className="pricing--link link">Pricing</a></li>
-                    <li className="get--started--li"><a href="#" className="get--started--link link">Get started for FREE</a></li>
-                    <li className="login--li"><a href="#" className="login--link link">Login</a></li>
+                    <li className="pricing--li"><a href="https://www.ecwid.com/pricing" className="pricing--link link">Pricing</a></li>
+                    <li className="get--started--li"><a href="https://my.ecwid.com/cp/#register" className="get--started--link link">Get started for FREE</a></li>
+                    <li className="login--li"><a href="https://my.ecwid.com/cp/" className="login--link link">Login</a></li>
                 </ul>
             </nav>
             <div className={`overlay ${openMenu ? "overlay--open" : ""}`}></div>
