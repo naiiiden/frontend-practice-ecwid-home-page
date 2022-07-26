7c8eb26dfaa0be88f5b4a35534ff78de64fef2f5
naiiiden <na4ko359@gmail.com>
2022-07-26 17:06:36 +0300

    hero component

---
+++ src/components/Hero.js
@@ -0,0 +1,17 @@
+import hero_illustration_mobile from "../images/illustrations/rectangle--26.webp";
+import hero_illustration_desktop from "../images/illustrations/rectangle--8.webp";
+
+const Hero = () => {
+    return (
+        <section className="hero--section">
+            <h1 className="hero--heading">Start selling online for free</h1>
+            <p className="hero--description">We’re putting free trials on trial. With Ecwid, you get free forever. Set up your free account once, and keep it as long as you like. Seriously.</p>
+            <a href="https://my.ecwid.com/cp/#register">Get started for free</a>
+            <p className="hero--small">No credit card required</p>
+            <img src={hero_illustration_desktop} alt="" className="hero--illustration"/>
+            <img src={hero_illustration_mobile} alt="" className="hero--illustration"/>
+        </section>
+    )
+}
+
+export default Hero;
