9e4702de3fa57d194a9104c52d418c906200e6f8
naiiiden <na4ko359@gmail.com>
2022-07-26 17:32:03 +0300

    hero component: add reviewer's rating & update img names for easier navigation

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,4 +1,7 @@
-import hero_illustration from "../images/illustrations/rectangle--8.webp";
+import hero_illustration from "../images/illustrations/hero-illustration.webp";
+import capterra from "../images/awards/capterra.webp";
+import trustpilot from "../images/awards/trustpilot.webp";
+import crowd from "../images/awards/crowd.webp";
 
 const Hero = () => {
     return (
@@ -8,6 +11,24 @@ const Hero = () => {
             <a href="https://my.ecwid.com/cp/#register">Get started for free</a>
             <p className="hero--small">No credit card required</p>
             <img src={hero_illustration} alt="" className="hero--illustration"/>
+            <div className="hero--ratings--container">
+                <figure className="hero--rating">
+                    <img src={capterra} alt="capterra's review" className="reviewer"/>
+                    <figcaption className="rater--opinion">Best ease of use</figcaption>
+                </figure>
+                <figure className="hero--rating">
+                    <img src={trustpilot} alt="trustpilot's review" className="reviewer"/>
+                    <figcaption className="rater--opinion">4.7 average rating</figcaption>
+                </figure>   
+                <figure className="hero--rating">
+                    <img src={crowd} alt="crowd's review" className="reviewer"/>
+                    <figcaption className="rater--opinion">Easiest Setup Summer 2021</figcaption>
+                </figure>
+                <figure className="hero--rating">
+                    <img src={crowd} alt="crowd's review" className="reviewer"/>
+                    <figcaption className="rater--opinion">Best Usability Summer 2021</figcaption>
+                </figure>
+            </div>
         </section>
     )
 }
