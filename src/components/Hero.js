a6b7fb728c5a1f4e7c0ef5a4bcd02da25eac1e05
naiiiden <na4ko359@gmail.com>
2022-07-26 17:15:05 +0300

    hero component: delete mobile illustration import & img

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,5 +1,4 @@
-import hero_illustration_mobile from "../images/illustrations/rectangle--26.webp";
-import hero_illustration_desktop from "../images/illustrations/rectangle--8.webp";
+import hero_illustration from "../images/illustrations/rectangle--8.webp";
 
 const Hero = () => {
     return (
@@ -8,8 +7,7 @@ const Hero = () => {
             <p className="hero--description">We’re putting free trials on trial. With Ecwid, you get free forever. Set up your free account once, and keep it as long as you like. Seriously.</p>
             <a href="https://my.ecwid.com/cp/#register">Get started for free</a>
             <p className="hero--small">No credit card required</p>
-            <img src={hero_illustration_desktop} alt="" className="hero--illustration"/>
-            <img src={hero_illustration_mobile} alt="" className="hero--illustration"/>
+            <img src={hero_illustration} alt="" className="hero--illustration"/>
         </section>
     )
 }
