2cd2216c659ecc99de772152308d0a7cac3cddfe
naiiiden <na4ko359@gmail.com>
2022-07-27 22:25:47 +0300

    delete unnecessary illustration images/rename images/import & render: content component 100% rendered props

--- src/App.js
+++ src/App.js
@@ -7,6 +7,13 @@ import instagram from "./images/instagram/instagram.webp";
 import michelle from "./images/people/michelle.webp";
 import pamela from "./images/people/pamela.webp";
 import benjamin from "./images/people/benjamin.webp";
+import trustpilot from "./images/awards/trustpilot.webp";
+import trustpilot_white from "./images/awards/trustpilot--white.webp";
+import stars_black from "./images/stars/stars--black.svg";
+import stars_white from "./images/stars/stars--white.webp";
+import content_illustration1 from "./images/illustrations/content-illustration-1.webp";
+import content_illustration2 from "./images/illustrations/content-illustration-2.webp";
+import content_illustration3 from "./images/illustrations/content-illustration-3.webp";
 
 const App = () => {
   return (
@@ -25,6 +32,10 @@ const App = () => {
           f2Desc="Let customers order directly from your Instagram posts when you’re powered by Ecwid."
           tAvatar={michelle}
           tName="Michelle Jackson"
+          tDesc="This is the perfect place to get your online business up and running. You literally don’t have to to anywhere else. They have everything you need plus add on widgets web syncing to social media and more!"
+          tReviewer={trustpilot}
+          tStars={stars_black}
+          cIllustration={content_illustration1}
         />
         <Content 
           hText="Grow faster" 
@@ -37,6 +48,10 @@ const App = () => {
           f2Desc="Create a Facebook campaign in minutes to reach up to two billion active Facebook users."
           tAvatar={pamela}
           tName="Pamela Strydom"
+          tDesc="Easy to use, affordable (and a free option if starting off). The platform integration with other programs such as Facebook, Instagram and local payment options make it an easy setup to manage effectively and easily."
+          tReviewer={trustpilot}
+          tStars={stars_black}
+          cIllustration={content_illustration2}
         />
         <Content 
           hText="Manage simply" 
@@ -49,6 +64,10 @@ const App = () => {
           f2Desc="Choose as many shipping rates and carriers as you like to provide you and your customers with the best experience."
           tAvatar={benjamin}
           tName="Benjamin Scott"
+          tDesc="Ecwid is awesome. Their smooth, operational and user friendly interface makes managing my store easy. They allow extensive and easy customization of products and variations galore."
+          tReviewer={trustpilot_white}
+          tStars={stars_white}
+          cIllustration={content_illustration3}
         />
       </main>
     </div>
