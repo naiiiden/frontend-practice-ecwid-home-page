d5b486486afb07669d24aa492c6598b04cbcbaf7
naiiiden <na4ko359@gmail.com>
2022-07-27 21:57:07 +0300

    content component add missing return () not rendering the component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import "./style/general.css";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
-
+import Content from "./components/Content";
 
 const App = () => {
   return (
@@ -9,6 +9,21 @@ const App = () => {
       <Header/>
       <main>
         <Hero/>
+        <Content 
+          hText="Sell anywhere" 
+          mainDesc="Set up your Ecwid store once to easily sync and sell across a website, social media, marketplaces like Amazon, and live in-person. Get started with one, or try them all."
+          href="https://www.ecwid.com/sell"
+        />
+        <Content 
+          hText="Grow faster" 
+          mainDesc="Ecwid gives you the power to easily sell anywhere, to anyone — across the internet and around the world. Control everything from a single platform with centralized inventory, order management, and pricing. It doesn’t get any simpler than this."
+          href="https://www.ecwid.com/promote"
+        />
+        <Content 
+          hText="Manage simply" 
+          mainDesc="Control everything seamlessly from a single dashboard with centralized inventory, order management, pricing, and more. Like a leisurely walk in the park…if that park was also made of money."
+          href="https://www.ecwid.com/manage"
+        />
       </main>
     </div>
   )
