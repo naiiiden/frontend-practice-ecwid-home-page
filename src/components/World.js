208485a2ed3081c5216d6279e97a4bfbcd592e41
naiiiden <na4ko359@gmail.com>
2022-07-30 14:42:52 +0300

    world component set max-width on 1600px+ and center horizontally

--- src/components/World.js
+++ src/components/World.js
@@ -1,25 +1,27 @@
 const World = () => {
     return (
-        <section className="world--section">
-            <div className="text--container">
-                <h2 className="world--heading">Sell around the world</h2>
-                <p className="world--desc">Take your small business global with built-in international payment tools and translation support for 50 languages - and counting.</p>
-            </div>
-            <div className="numbers--container">
-                <div className="number--subcontainer">
-                    <h3 className="number">50+</h3>
-                    <p className="small-text">Payment gateways</p>
+        <div className="big-width">
+            <section className="world--section">
+                <div className="text--container">
+                    <h2 className="world--heading">Sell around the world</h2>
+                    <p className="world--desc">Take your small business global with built-in international payment tools and translation support for 50 languages - and counting.</p>
                 </div>
-                <div className="number--subcontainer">
-                    <h3 className="number">175</h3>
-                    <p className="small-text">Countries</p>
-                </div> 
-                <div className="number--subcontainer">
-                    <h3 className="number">50</h3>
-                    <p className="small-text">Languages</p>
+                <div className="numbers--container">
+                    <div className="number--subcontainer">
+                        <h3 className="number">50+</h3>
+                        <p className="small-text">Payment gateways</p>
+                    </div>
+                    <div className="number--subcontainer">
+                        <h3 className="number">175</h3>
+                        <p className="small-text">Countries</p>
+                    </div> 
+                    <div className="number--subcontainer">
+                        <h3 className="number">50</h3>
+                        <p className="small-text">Languages</p>
+                    </div>
                 </div>
-            </div>
-        </section>
+            </section>
+        </div>
     )
 }
 
