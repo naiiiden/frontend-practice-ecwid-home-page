4e3b7dac68931519b8b783fc71910d87bd74fadf
naiiiden <na4ko359@gmail.com>
2022-07-30 14:51:34 +0300

    world component import decorative shapes and position them on mobile

--- src/components/World.js
+++ src/components/World.js
@@ -1,7 +1,14 @@
+import square from "../images/shapes/bitmap--3.webp";
+import triangle1 from "../images/triangles/bitmap--4.webp";
+import triangle2 from "../images/triangles/bitmap--5.webp";
+
 const World = () => {
     return (
         <div className="big-width">
             <section className="world--section">
+                <img src={square} alt="" className="shape"/>
+                <img src={triangle1} alt="" className="shape"/>
+                <img src={triangle2} alt="" className="shape"/>
                 <div className="text--container">
                     <h2 className="world--heading">Sell around the world</h2>
                     <p className="world--desc">Take your small business global with built-in international payment tools and translation support for 50 languages - and counting.</p>
