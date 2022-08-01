d361f63401cf29d0624db7b7d74eec4b0a1369c6
naiiiden <na4ko359@gmail.com>
2022-08-01 07:11:06 +0300

    cta component styled on 1024px+

--- src/components/Cta.js
+++ src/components/Cta.js
@@ -2,8 +2,10 @@ const Cta = () => {
     return (
         <article className="cta--container">
             <h4 className="cta--heading">Your free online store is just a few clicks away</h4>
-            <a href="https://my.ecwid.com/cp/#register" className="cta--link">Get started for free →</a>
-            <p className="cta--p">No credit card required</p>
+            <div className="cta--subcontainer">
+                <a href="https://my.ecwid.com/cp/#register" className="cta--link">Get started for free →</a>
+                <p className="cta--p">No credit card required</p>
+            </div>
         </article>
     )
 }
