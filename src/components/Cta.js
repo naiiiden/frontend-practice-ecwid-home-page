445ea46454d71fd3d61edf350a32e088517feda2
naiiiden <na4ko359@gmail.com>
2022-08-01 07:23:13 +0300

    import decorative images and hide them on mobile/tablet

--- src/components/Cta.js
+++ src/components/Cta.js
@@ -1,7 +1,12 @@
+import circle from "../images/shapes/bitmap--1.webp";
+import triangle from "../images/shapes/bitmap.webp";
+
 const Cta = () => {
     return (
         <div className="big-width">
             <article className="cta--container">
+                <img src={circle} alt="" className="shape"/>
+                <img src={triangle} alt="" className="shape"/>
                 <h4 className="cta--heading">Your free online store is just a few clicks away</h4>
                 <div className="cta--subcontainer">
                     <a href="https://my.ecwid.com/cp/#register" className="cta--link">Get started for free →</a>
