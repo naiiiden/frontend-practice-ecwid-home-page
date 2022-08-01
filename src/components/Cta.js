4872488a86691feadeabe9a3161171f9d16e6563
naiiiden <na4ko359@gmail.com>
2022-08-01 07:17:20 +0300

    cta component wrapped inside div set max-width and center horizontally on 1600px+

--- src/components/Cta.js
+++ src/components/Cta.js
@@ -1,12 +1,14 @@
 const Cta = () => {
     return (
-        <article className="cta--container">
-            <h4 className="cta--heading">Your free online store is just a few clicks away</h4>
-            <div className="cta--subcontainer">
-                <a href="https://my.ecwid.com/cp/#register" className="cta--link">Get started for free →</a>
-                <p className="cta--p">No credit card required</p>
-            </div>
-        </article>
+        <div className="big-width">
+            <article className="cta--container">
+                <h4 className="cta--heading">Your free online store is just a few clicks away</h4>
+                <div className="cta--subcontainer">
+                    <a href="https://my.ecwid.com/cp/#register" className="cta--link">Get started for free →</a>
+                    <p className="cta--p">No credit card required</p>
+                </div>
+            </article>
+        </div>
     )
 }
 
