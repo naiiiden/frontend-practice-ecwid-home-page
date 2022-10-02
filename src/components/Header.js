0d547ea8528bcabdef2d81b33ce81358da5cebec
naiiiden <na4ko359@gmail.com>
2022-10-02 08:47:21 +0300

    https://w3c.github.io/aria-practices/examples/menubar/menubar-navigation.html

--- src/components/Header.js
+++ src/components/Header.js
@@ -52,93 +52,92 @@ const Header = () => {
             </button>
             <nav className="header--nav">
                 {isTablet && !isDesktop ? 
-                <ul className="nav768-1280--ul">
-                    <li><a href="https://www.ecwid.com/sell">Sell</a></li>
-                    <li><a href="https://www.ecwid.com/promote">Market</a></li>
-                    <li><a href="https://www.ecwid.com/manage">Manage</a></li>
-                    <li className="get--started--li2"><a href="https://my.ecwid.com/cp/#register">Get Started for FREE</a></li>
+                <ul role="menubar" className="nav768-1280--ul">
+                    <li role="none"><a role="menuitem" href="https://www.ecwid.com/sell">Sell</a></li>
+                    <li role="none"><a role="menuitem" href="https://www.ecwid.com/promote">Market</a></li>
+                    <li role="none"><a role="menuitem" href="https://www.ecwid.com/manage">Manage</a></li>
+                    <li role="none" className="get--started--li2"><a role="menuitem" href="https://my.ecwid.com/cp/#register">Get Started for FREE</a></li>
                 </ul> : ""}
-                <ul className={`header--nav--ul ${openMenu ? "show" : ""}`}>
+                <ul className={`header--nav--ul ${openMenu ? "show" : ""}`} role="menubar">
                 {!isDesktop ? 
-                    <li>
-                        <span onClick={() => setOpenSell(!openSell)} className={`${openSell ? "open" : ""}`} role="button" aria-haspopup="true" aria-expanded={openSell ? "true" : "false"}>Sell</span>
-                        <ul className={`dropdown ${openSell ? "show" : ""}`}>
-                            <li><a href="https://www.ecwid.com/sell">Sell anywhere</a></li>
-                            <li><a href="https://www.ecwid.com/sell">Website</a></li>
-                            <li><a href="https://www.ecwid.com/instagram">Instagram</a></li>
-                            <li><a href="https://www.ecwid.com/tiktok">TikTok</a></li>
-                            <li><a href="https://www.ecwid.com/facebook-commerce">Facebook</a></li>
-                            <li><a href="https://www.ecwid.com/sell-on-google">Google</a></li>
-                            <li><a href="https://www.ecwid.com/amazon">Amazon</a></li>
-                            <li><a href="https://www.ecwid.com/pos">POS</a></li>
-                            <li><a href="https://www.ecwid.com/shopapp">ShopApp</a></li>
+                    <li role="none">
+                        <span onClick={() => setOpenSell(!openSell)} className={`${openSell ? "open" : ""}`} role="menuitem" aria-haspopup="true" aria-expanded={openSell ? "true" : "false"}>Sell</span>
+                        <ul className={`dropdown ${openSell ? "show" : ""}`} role="menu">
+                            <li role="none"><a role="menuitem" href="https://www.ecwid.com/sell">Sell anywhere</a></li>
+                            <li role="none"><a role="menuitem" href="https://www.ecwid.com/sell">Website</a></li>
+                            <li role="none"><a role="menuitem" href="https://www.ecwid.com/instagram">Instagram</a></li>
+                            <li role="none"><a role="menuitem" href="https://www.ecwid.com/tiktok">TikTok</a></li>
+                            <li role="none"><a role="menuitem" href="https://www.ecwid.com/facebook-commerce">Facebook</a></li>
+                            <li role="none"><a role="menuitem" href="https://www.ecwid.com/sell-on-google">Google</a></li>
+                            <li role="none"><a role="menuitem" href="https://www.ecwid.com/amazon">Amazon</a></li>
+                            <li role="none"><a role="menuitem" href="https://www.ecwid.com/pos">POS</a></li>
+                            <li role="none"><a role="menuitem" href="https://www.ecwid.com/shopapp">ShopApp</a></li>
                         </ul>
                     </li>
                     :
-                    <li><a href="https://www.ecwid.com/sell">Sell</a></li>
+                    <li role="none"><a role="menuitem" href="https://www.ecwid.com/sell">Sell</a></li>
                 }
                 {!isDesktop ? 
-                    <li>
-                        <span onClick={() => setOpenMarket(!openMarket)} className={`${openMarket ? "open" : ""}`} role="button" aria-haspopup="true" aria-expanded={openMarket ? "true" : "false"}>Market</span>
-                        <ul className={`dropdown ${openMarket ? "show" : ""}`}>
-                            <li><a href="https://www.ecwid.com/promote">Market your business</a></li>
-                            <li><a href="https://www.ecwid.com/google-advertising">Google Ads</a></li>
-                            <li><a href="https://www.ecwid.com/facebook-remarketing">Facebook</a></li>
-                            <li><a href="https://www.ecwid.com/mailchimp">Email</a></li>
-
+                    <li role="none">
+                        <span onClick={() => setOpenMarket(!openMarket)} className={`${openMarket ? "open" : ""}`} role="menuitem" aria-haspopup="true" aria-expanded={openMarket ? "true" : "false"}>Market</span>
+                        <ul className={`dropdown ${openMarket ? "show" : ""}`} role="menu">
+                            <li role="none"><a role="menuitem" href="https://www.ecwid.com/promote">Market your business</a></li>
+                            <li role="none"><a role="menuitem" href="https://www.ecwid.com/google-advertising">Google Ads</a></li>
+                            <li role="none"><a role="menuitem" href="https://www.ecwid.com/facebook-remarketing">Facebook</a></li>
+                            <li role="none"><a role="menuitem" href="https://www.ecwid.com/mailchimp">Email</a></li>
                         </ul>
                     </li>
                     :
-                    <li><a href="https://www.ecwid.com/promote">Market</a></li>
+                    <li role="none"><a role="menuitem" href="https://www.ecwid.com/promote">Market</a></li>
                 }
                 {!isDesktop ? 
-                    <li>
-                        <span onClick={() => setOpenManage(!openManage)} className={`${openManage ? "open" : ""}`} role="button" aria-haspopup="true" aria-expanded={openManage ? "true" : "false"}>Manage</span>
-                        <ul className={`dropdown ${openManage ? "show" : ""}`}>
-                            <li><a href="https://www.ecwid.com/manage">Manage your store</a></li>
-                            <li><a href="https://www.ecwid.com/ecwid-mobile">Mobile</a></li>
-                            <li><a href="https://www.ecwid.com/apps">App Market</a></li>
-                            <li><a href="https://www.ecwid.com/shipping-labels">Discount Shipping</a></li>
+                    <li role="none">
+                        <span onClick={() => setOpenManage(!openManage)} className={`${openManage ? "open" : ""}`} role="menuitem" aria-haspopup="true" aria-expanded={openManage ? "true" : "false"}>Manage</span>
+                        <ul className={`dropdown ${openManage ? "show" : ""}`} role="menu">
+                            <li role="none"><a role="menuitem" href="https://www.ecwid.com/manage">Manage your store</a></li>
+                            <li role="none"><a role="menuitem" href="https://www.ecwid.com/ecwid-mobile">Mobile</a></li>
+                            <li role="none"><a role="menuitem" href="https://www.ecwid.com/apps">App Market</a></li>
+                            <li role="none"><a role="menuitem" href="https://www.ecwid.com/shipping-labels">Discount Shipping</a></li>
                         </ul>
                     </li>
                     :
-                    <li><a href="https://www.ecwid.com/manage">Manage</a></li>
+                    <li role="none"><a role="menuitem" href="https://www.ecwid.com/manage">Manage</a></li>
                 }
                 {!isDesktop ? 
-                    <li>
-                        <span onClick={() => setOpenLearn(!openLearn)} className={`${openLearn ? "open" : ""}`} role="button" aria-haspopup="true" aria-expanded={openLearn ? "true" : "false"}>Learn</span>
-                        <ul className={`dropdown ${openLearn ? "show" : ""}`}>
-                            <li><a href="https://www.ecwid.com/blog">Learn</a></li>
-                            <li><a href="https://www.ecwid.com/watchdemo">Demo</a></li>
-                            <li><a href="https://support.ecwid.com/hc/en-us">Help Center</a></li>
-                            <li><a href="https://www.ecwid.com/blog">Blog</a></li>
-                            <li><a href="https://support.ecwid.com/hc/en-us/requests/new">Customer Support</a></li>
-                            <li><a href="https://www.ecwid.com/guides">Guides</a></li>
-                            <li><a href="https://ecommerce-academy.ecwid.com/">Ecwid Academy</a></li>
-                            <li><a href="https://www.ecwid.com/blog/podcast">Podcast</a></li>
-                            <li><a href="https://support.ecwid.com/hc/en-us/categories/360000015889-Ecwid-Video-Guides">Tutorials</a></li>
+                    <li role="none">
+                        <span onClick={() => setOpenLearn(!openLearn)} className={`${openLearn ? "open" : ""}`} role="menuitem" aria-haspopup="true" aria-expanded={openLearn ? "true" : "false"}>Learn</span>
+                        <ul className={`dropdown ${openLearn ? "show" : ""}`} role="menu">
+                            <li role="none"><a role="menuitem" href="https://www.ecwid.com/blog">Learn</a></li>
+                            <li role="none"><a role="menuitem" href="https://www.ecwid.com/watchdemo">Demo</a></li>
+                            <li role="none"><a role="menuitem" href="https://support.ecwid.com/hc/en-us">Help Center</a></li>
+                            <li role="none"><a role="menuitem" href="https://www.ecwid.com/blog">Blog</a></li>
+                            <li role="none"><a role="menuitem" href="https://support.ecwid.com/hc/en-us/requests/new">Customer Support</a></li>
+                            <li role="none"><a role="menuitem" href="https://www.ecwid.com/guides">Guides</a></li>
+                            <li role="none"><a role="menuitem" href="https://ecommerce-academy.ecwid.com/">Ecwid Academy</a></li>
+                            <li role="none"><a role="menuitem" href="https://www.ecwid.com/blog/podcast">Podcast</a></li>
+                            <li role="none"><a role="menuitem" href="https://support.ecwid.com/hc/en-us/categories/360000015889-Ecwid-Video-Guides">Tutorials</a></li>
                         </ul>
                     </li>
                     :
-                    <li><a href="https://www.ecwid.com/blog">Learn</a></li>
+                    <li role="none"><a role="menuitem" href="https://www.ecwid.com/blog">Learn</a></li>
                 }
                 {!isDesktop ? 
-                    <li>
-                        <span onClick={() => setOpenPartners(!openPartners)} className={`${openPartners ? "open" : ""}`} role="button" aria-haspopup="true" aria-expanded={openPartners ? "true" : "false"}>Partners</span>
-                        <ul className={`dropdown ${openPartners ? "show" : ""}`}>
-                            <li><a href="https://www.ecwid.com/partners">Partners <WindowIcon className="window--icon"/></a></li>
-                            <li><a href="https://www.ecwid.com/partners/reseller">Resell <WindowIcon className="window--icon"/></a></li>
-                            <li><a href="https://www.ecwid.com/partners/referral">Refer <WindowIcon className="window--icon"/></a></li>
-                            <li><a href="https://www.ecwid.com/partners/app-market">Build an App <WindowIcon className="window--icon"/></a></li>
-                            <li><a href="https://www.ecwid.com/experts">Find an expert <WindowIcon className="window--icon"/></a></li>
+                    <li role="none">
+                        <span onClick={() => setOpenPartners(!openPartners)} className={`${openPartners ? "open" : ""}`} role="menuitem" aria-haspopup="true" aria-expanded={openPartners ? "true" : "false"}>Partners</span>
+                        <ul className={`dropdown ${openPartners ? "show" : ""}`} role="menu">
+                            <li role="none"><a role="menuitem" href="https://www.ecwid.com/partners">Partners <WindowIcon className="window--icon"/></a></li>
+                            <li role="none"><a role="menuitem" href="https://www.ecwid.com/partners/reseller">Resell <WindowIcon className="window--icon"/></a></li>
+                            <li role="none"><a role="menuitem" href="https://www.ecwid.com/partners/referral">Refer <WindowIcon className="window--icon"/></a></li>
+                            <li role="none"><a role="menuitem" href="https://www.ecwid.com/partners/app-market">Build an App <WindowIcon className="window--icon"/></a></li>
+                            <li role="none"><a role="menuitem" href="https://www.ecwid.com/experts">Find an expert <WindowIcon className="window--icon"/></a></li>
                         </ul>
                     </li>
                     :
-                    <li><a href="https://www.ecwid.com/partners">Partners</a></li>
+                    <li role="none"><a role="menuitem" href="https://www.ecwid.com/partners">Partners</a></li>
                 }
-                    <li className="pricing--li"><a href="https://www.ecwid.com/pricing" className="pricing--link link">Pricing</a></li>
-                    <li className="get--started--li"><a href="https://my.ecwid.com/cp/#register" className="get--started--link link">Get started for FREE</a></li>
-                    <li className="login--li"><a href="https://my.ecwid.com/cp/" className="login--link link">Login</a></li>
+                    <li role="none" className="pricing--li"><a role="menuitem" href="https://www.ecwid.com/pricing" className="pricing--link link">Pricing</a></li>
+                    <li role="none" className="get--started--li"><a role="menuitem" href="https://my.ecwid.com/cp/#register" className="get--started--link link">Get started for FREE</a></li>
+                    <li role="none" className="login--li"><a role="menuitem" href="https://my.ecwid.com/cp/" className="login--link link">Login</a></li>
                 </ul>
             </nav>
             <div className={`overlay ${openMenu ? "overlay--open" : ""}`}></div>
